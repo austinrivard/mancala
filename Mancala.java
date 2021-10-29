355316038e224fed4adbf8edc0165bea80ebf1c3
Robert Yav <robert.yav@sjsu.edu>
2021-10-29 16:44:28 -0700

    Skeleton

--- Mancala.java
+++ Mancala.java
@@ -1,8 +1,11 @@
+import java.util.*;
 /**
  * Mancala - a game in which two players move stones around the board
  * with the goal of getting the most number of stones in their mancala.
  */
 
 public class Mancala {
-    // test - Robert
+    private boolean playerTurn;
+    private ArrayList<Pit> pitList;
+    
 }
