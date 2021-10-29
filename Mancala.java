cf245f1bf5ab7731c7e1d12cc4fee9fed9a23a7e
austinrivard <56987226+austinrivard@users.noreply.github.com>
2021-10-29 00:45:06 -0700

    Add files

---
+++ Mancala.java
@@ -0,0 +1,8 @@
+/**
+ * Mancala - a game in which two players move stones around the board
+ * with the goal of getting the most number of stones in their mancala.
+ */
+
+public class Mancala {
+    
+}
---
+++ MancalaTest.java
@@ -0,0 +1,13 @@
+/**
+ * Provides a user interface to play Mancala.
+ * 
+ * @author Austin Rivard
+ * @author Robert Yav
+ * @author Brendan Requierme
+ */
+
+public class MancalaTest {
+    public static void main(String[] args) {
+        // Mancala model = new Mancala();
+    }
+}
