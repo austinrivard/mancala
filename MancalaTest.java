113b585e0a4ca85c158c473fb06b8ae0c9028364
Austin Rivard <56987226+austinrivard@users.noreply.github.com>
2021-11-19 18:16:26 -0800

    Update settings menu

--- MancalaTest.java
+++ MancalaTest.java
@@ -11,55 +11,69 @@ import java.awt.*;
 
 public class MancalaTest {
     public static void main(String[] args) {
-        JFrame frame = new JFrame();
+        JFrame frame = new JFrame("Mancala Settings");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLocation(200, 200);
+        frame.setPreferredSize(new Dimension(300, 400));
 
-        Integer[] numStoneOptions = {3, 4};
-        int choice = JOptionPane.showInternalOptionDialog(frame,
-                                                        "Number of starting stones",
-                                                        "Settings",
-                                                        JOptionPane.DEFAULT_OPTION,
-                                                        JOptionPane.PLAIN_MESSAGE,
-                                                        null,
-                                                        numStoneOptions,
-                                                        null);
-        int numStones = numStoneOptions[choice];
+        JPanel settingsPanel = new JPanel();
 
-        ImageIcon[] mancalaStyles = new ImageIcon[2];
-        mancalaStyles[0] = new ImageIcon("img/mancala0.png");
-        mancalaStyles[1] = new ImageIcon("img/mancala1.png");
+        JRadioButton[] numStonesButtons = new JRadioButton[2];
+        numStonesButtons[0] = new JRadioButton("3");
+        numStonesButtons[1] = new JRadioButton("4");
+        ButtonGroup numStonesGroup = new ButtonGroup();
+        for (JRadioButton btn : numStonesButtons) {
+            numStonesGroup.add(btn);        
+            settingsPanel.add(btn);
+        }
 
-        ImageIcon[] pitsStyles = new ImageIcon[2];
-        pitsStyles[0] = new ImageIcon("img/pits0.png");
-        pitsStyles[1] = new ImageIcon("img/pits1.png");
+        JRadioButton[] mancalaStylesButtons = new JRadioButton[2];
+        mancalaStylesButtons[0] = new JRadioButton(new ImageIcon("img/mancala0.png"));
+        mancalaStylesButtons[1] = new JRadioButton(new ImageIcon("img/mancala1.png"));
+        ButtonGroup mancalaStylesGroup = new ButtonGroup();
+        for (JRadioButton btn : mancalaStylesButtons) {
+            mancalaStylesGroup.add(btn);
+            settingsPanel.add(btn);
+        }
 
-        choice = JOptionPane.showInternalOptionDialog(frame,
-                                                     "Choose mancala style",
-                                                     "",
-                                                     JOptionPane.DEFAULT_OPTION,
-                                                     JOptionPane.PLAIN_MESSAGE,
-                                                     null,
-                                                     mancalaStyles,
-                                                     null);
-        Icon mancalaIcon = mancalaStyles[choice];                               
+        JRadioButton[] pitsStylesButtons = new JRadioButton[2];
+        pitsStylesButtons[0] = new JRadioButton(new ImageIcon("img/pits0.png"));
+        pitsStylesButtons[1] = new JRadioButton(new ImageIcon("img/pits1.png"));
+        ButtonGroup pitsStylesGroup = new ButtonGroup();
+        for (JRadioButton btn : pitsStylesButtons) {
+            pitsStylesGroup.add(btn);
+            settingsPanel.add(btn);
+        }
 
-        choice = JOptionPane.showInternalOptionDialog(frame,
-                                                     "Choose pits style",
-                                                     "",
-                                                     JOptionPane.DEFAULT_OPTION,
-                                                     JOptionPane.PLAIN_MESSAGE,
-                                                     null,
-                                                     pitsStyles,
-                                                     null);
-        Icon pitsIcon = pitsStyles[choice];
+        JButton startButton = new JButton("Start Game");
+        startButton.addActionListener(event -> {
+            int numStones = 0;
+            for (JRadioButton btn : numStonesButtons) if (btn.isSelected()) numStones = Integer.parseInt(btn.getText());
 
+            Icon mancalaIcon = null;
+            for (JRadioButton btn : mancalaStylesButtons) if (btn.isSelected()) mancalaIcon = btn.getIcon();
+
+            Icon pitsIcon = null;
+            for (JRadioButton btn : pitsStylesButtons) if (btn.isSelected()) pitsIcon = btn.getIcon();
+
+            if (numStones != 0 && mancalaIcon != null && pitsIcon != null) {
+                frame.dispose();
+                startGame(numStones, mancalaIcon, pitsIcon);
+            }
+        });
+        settingsPanel.add(startButton);
+
+        frame.add(settingsPanel);
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+    private static void startGame(int numStones, Icon mancalaIcon, Icon pitsIcon) {
         Style style = new Style() {
             public MancalaPanel mancalaPanel() { return new MancalaPanel(mancalaIcon); }
             public PitsPanel pitsPanel() { return new PitsPanel(pitsIcon); }
         };
-
-        frame.dispose();
-
+        
         Mancala game = new Mancala(numStones);
         MancalaBoard board = new MancalaBoard(game, style);
     }
